0c2fb14170ace397a166e25820ce130df64139be
czechart <44610660+czechart@users.noreply.github.com>
2019-01-20 11:18:29 +0100

    Update hci.py

--- hci.py
+++ hci.py
@@ -1,28 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import aseegg as ag
-import matplotlib.patches as mpatches
-import matplotlib.pyplot as plt
 import pandas as pd
 
-pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', None) 
 
 dane = pd.read_csv(r"/Users/dawidczech/Desktop/dane2.csv", delimiter=';', engine='python') 
 kanal1=dane["pierwsza"]
 numer=dane["Liczba"]
 
-#7603
-
 czestProbkowania = 200
 t=np.linspace(0, 37.96, 200*37.96)
+
+przefiltrowany1 = ag.pasmowozaporowy(kanal1, czestProbkowania, 49,51)
+przefiltrowany2 = ag.pasmowoprzepustowy(przefiltrowany1, czestProbkowania, 1,50)
 
 plt.subplot(2, 1, 1)
 plt.plot(t,kanal1[:int(200*37.96)]) #to wyświetla pełen sygnał naszej osoby
 plt.xlabel('Czas [s]')
 plt.ylabel('Amplituda [uV]')
-
-przefiltrowany1 = ag.pasmowozaporowy(kanal1, czestProbkowania, 49,51)
-przefiltrowany2 = ag.pasmowoprzepustowy(przefiltrowany1, czestProbkowania, 1,50)
 
 plt.subplot(2, 1, 2)
 plt.plot(t,przefiltrowany2[:int(200*37.96)]) #to wyświetla przefiltrowany
@@ -31,12 +27,9 @@ plt.ylabel('Amplituda [uV]')
 
 plt.show()
 
-x=7593
-y=0.1 #wartość progowa mrugnięcia
-
 print("Osoba badana mrugnęła przy tych cyferkach: ")
 
-for i in range (x):
-    if przefiltrowany2[i]>y:
+for i in range (7593):
+    if przefiltrowany2[i]>0.1: #wartość progowa mrugnięcia
         print(numer[i])
 
